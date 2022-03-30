87134ac0e73b665dc3161716ddde3af2181cd9b9
Rav4ppz <s550robert@icloud.com>
2022-03-29 19:51:14 -0700

    Initial Commit

---
+++ SwiftUItest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUItest
+//
+//  Created by Robert C on 3/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUItest/SwiftUItestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUItestApp.swift
+//  SwiftUItest
+//
+//  Created by Robert C on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUItestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
